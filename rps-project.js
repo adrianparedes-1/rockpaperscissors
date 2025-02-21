a3947a45cf9349f5652a3a5a381f2b7c3ef19095
Adrian Paredes <adrianparedes@Adrians-MacBook-Pro.local>
2025-02-21 16:00:32 -0500

    Initial commit

---
+++ rps-project.js
@@ -0,0 +1,11 @@
+/*
+Create a Rock-paper-scissors game that you can play with the computer. The computer will make a random choice and there will be 5 rounds. Once the game is over,
+show the winner. 
+
+Understand:
+
+Plan: 
+
+Implement:
+
+*/
